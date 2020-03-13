1b055b262169dc9b68a5e23928dfd22f36bc94e3
Hatem <hatem.saturi@gmail.com>
2020-03-13 20:49:35 +0100

    WIP Contact Form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,42 +1,75 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './Contact.module.css'
 import { LanguageContext } from '../../context/context'
+import axios from 'axios'
 
-const Contact = (props) => {
+class Contact extends Component {
 
-    const formHandler = (e) => {
+    state = {
+        form: {
+            name: {
+                value: ''
+            },
+            email: {
+                value: ''
+            },
+            description: {
+                value: ''
+            }
+        }
+    }
+    formHandler = (e) => {
         e.preventDefault()
+        // console.log(e.target[0].value);
+        // console.log(e.target[1].value);
+        // console.log(e.target[2].value);
+        // if (!e.target[0].value) {
+        //     console.log('vide');
+        // } else {
+        //     console.log('rempli');
+
+        // }
+        console.log(this.state);
+
+
     }
 
-    return (
-        <LanguageContext.Consumer>
-            {({ lang }) => {
-                return (
-                    <div className={classes.Contact}>
-                        <form action="">
-                            <h3>{lang.contact.call}</h3>
-                            <div>
-                                {/* <label htmlFor="">Email</label> */}
-                                <input type="text" placeholder="Email" name="" id="" />
-                            </div>
-                            <div>
-                                {/* <label htmlFor="">Name</label> */}
-                                <input type="text" placeholder={lang.contact.name} name="" id="" />
+    changeHandler = (e) => {
+        const name = e.target.name
+        const value = e.target.value
 
-                            </div>
-                            <div>
-                                {/* <label htmlFor="">Message</label> */}
-                                <textarea rows="6" placeholder="Message" ></textarea>
-                            </div>
-                            <div className={classes.Button}>
-                                <button onClick={formHandler}>{lang.contact.send}</button>
-                            </div>
-                        </form>
-                    </div>
-                )
-            }}
-        </LanguageContext.Consumer>
-    )
+        this.setState({})
+    }
+    render() {
+
+        return (
+            <LanguageContext.Consumer>
+                {({ lang }) => {
+                    return (
+                        <div className={classes.Contact}>
+                            <form onSubmit={this.formHandler}>
+                                <h3>{lang.contact.call}</h3>
+                                <div>
+                                    <input type="text" placeholder="Email" name="email" onChange={this.changeHandler} />
+                                </div>
+                                <div>
+                                    <input type="text" placeholder={lang.contact.name} name="name" onChange={this.changeHandler} />
+
+                                </div>
+                                <div>
+                                    <textarea rows="6" placeholder="Message" name="description" onChange={this.changeHandler}></textarea>
+                                </div>
+                                <div className={classes.Button}>
+                                    <button disabled={false} type="submit" >{lang.contact.send}</button>
+                                </div>
+                            </form>
+                        </div>
+                    )
+                }}
+            </LanguageContext.Consumer >
+        )
+    }
+
 }
 
 export default Contact
