8b4142a071cf89b05bfdf3f0815a2ef5f2c3befe
Hatem <hatem.saturi@gmail.com>
2020-03-15 18:30:19 +0100

    Contac Form & Firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,48 +1,52 @@
 const functions = require('firebase-functions');
 const express = require('express')
-const nodemailer = require("nodemailer");
-require('dotenv').config()
-// const cors = require('cors')
+const cors = require('cors')
 const app = express()
 
-const bodyParser = require('body-parser')
-
-// app.use(cors())
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-const mail = app.post('/', async function (req, res) {
-    console.log(req.body)
-    console.log(process.env.USER);
-    console.log(process.env.PASSWORD);
-    console.log(process.env.SERVICE);
-
-    let transporter = nodemailer.createTransport({
-        service: process.env.SERVICE,
-        auth: {
-            user: process.env.USER,
-            pass: process.env.PASSWORD
-        }
-    });
-
-    await transporter.sendMail({
-        to: process.env.TO,
-        subject: `Message from ${req.body.name}`,
-        html: `<h3>${req.body.email}</h3>
-                <p>${req.body.message}</p>`
-    }).catch(err => console.log(err))
-
-    res.json({ hello: "worldzzz" })
+const mailHandler = app.post('/', async function (req, res) {
+    const config = functions.config()
+    const mailjet = require('node-mailjet')
+        .connect(config.env.mailuser, config.env.maillogin)
+    const request = mailjet
+        .post("send", { 'version': 'v3.1' })
+        .request({
+            "Messages": [
+                {
+                    "From": {
+                        "Email": config.env.user,
+                        "Name": req.body.name
+                    },
+                    "To": [
+                        {
+                            "Email": config.env.user,
+                            "Name": "Hatem"
+                        }
+                    ],
+                    "Subject": `New message from ${req.body.name} - ${req.body.email}`,
+                    "TextPart": req.body.message,
+                }
+            ]
+        })
+    request
+        .then((result) => {
+            console.log(result.body)
+            res.json({ message: "Email Sent" })
+        })
+        .catch((err) => {
+            console.log(err.statusCode)
+        })
 })
 
 
-const mailHandler = functions
+const mail = functions
     .region('europe-west1')
     .runWith({ memory: '128MB', timeoutSeconds: 20 })
-    .https.onRequest(mail)
+    .https.onRequest(mailHandler)
 
 module.exports = {
-    mailHandler,
+    mail
 }
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -8,37 +8,84 @@ class Contact extends Component {
     state = {
         form: {
             name: {
-                value: ''
+                value: '',
+                valid: false
             },
             email: {
-                value: ''
+                value: '',
+                valid: false
             },
-            description: {
-                value: ''
+            message: {
+                value: '',
+                valid: false
+            },
+            button: {
+                disabled: true
             }
         }
     }
     formHandler = (e) => {
         e.preventDefault()
-        // console.log(e.target[0].value);
-        // console.log(e.target[1].value);
-        // console.log(e.target[2].value);
-        // if (!e.target[0].value) {
-        //     console.log('vide');
-        // } else {
-        //     console.log('rempli');
+        const url = 'https://europe-west1-portfoliohatemsat.cloudfunctions.net/mail'
 
-        // }
-        console.log(this.state);
-
-
+        axios.post(url, {
+            email: this.state.form.email.value,
+            name: this.state.form.name.value,
+            message: this.state.form.message.value
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     changeHandler = (e) => {
         const name = e.target.name
         const value = e.target.value
+        
+        const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        let valid, buttonDisabled
 
-        this.setState({})
+        switch (name) {
+            case 'email':
+                valid = emailRegex.test(value)
+                break;
+
+            case 'name':
+                valid = value ? true : false
+                break;
+
+            case 'message':
+                valid = value ? true : false
+                break;
+            default:
+                break;
+        }
+
+        this.setState({
+            form: {
+                ...this.state.form, [name]: {
+                    ...this.state.form[name], value, valid
+                }
+            }
+        }, () => {
+            if (this.state.form.email.valid && this.state.form.name.valid && this.state.form.message.valid) {
+                buttonDisabled = false
+            } else {
+                buttonDisabled = true
+            }
+            this.setState({
+                form: {
+                    ...this.state.form, button: {
+                        disabled: buttonDisabled
+                    }
+                }
+            })
+
+        })
+
     }
     render() {
 
@@ -53,14 +100,14 @@ class Contact extends Component {
                                     <input type="text" placeholder="Email" name="email" onChange={this.changeHandler} />
                                 </div>
                                 <div>
-                                    <input type="text" placeholder={lang.contact.name} name="name" onChange={this.changeHandler} />
+                                    <input type="text" value={this.state.form.name.value} placeholder={lang.contact.name} name="name" onChange={this.changeHandler} />
 
                                 </div>
                                 <div>
-                                    <textarea rows="6" placeholder="Message" name="description" onChange={this.changeHandler}></textarea>
+                                    <textarea rows="6" placeholder="Message" name="message" onChange={this.changeHandler}></textarea>
                                 </div>
                                 <div className={classes.Button}>
-                                    <button disabled={false} type="submit" >{lang.contact.send}</button>
+                                    <button disabled={this.state.form.button.disabled} type="submit" >{lang.contact.send}</button>
                                 </div>
                             </form>
                         </div>
